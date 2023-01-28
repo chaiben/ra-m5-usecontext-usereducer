dd6d4586fec8e6434c4513956d1ed0b63491080d
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2023-01-28 13:55:50 +0100

    Removed external component

--- src/components/organisms/ITATable/ItemsPerPage.jsx
+++ src/components/organisms/ITATable/ItemsPerPage.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { TableContext } from './store/context'
 import { Actions } from './store/reducer'
-import { SelectGroup } from '../../molecules'
+import FlexBox from './Flexbox'
 
-const SelectGroupStyled = styled(SelectGroup)`
+const SelectGroupStyled = styled.select`
   padding: 0rem 0.5rem;
   border-radius: 4px;
   margin-right: 1rem;
@@ -13,23 +13,30 @@ const SelectGroupStyled = styled(SelectGroup)`
 function ItemsPerPage() {
   const { state, dispatch } = useContext(TableContext)
   const { itemsPerPage } = state
+  const id = 'itemsPerPage'
 
   return (
-    <SelectGroupStyled
-      id="itemsPerPage"
-      label="Mostrar"
-      value={itemsPerPage.toString()}
-      options={['10', '25', '50'].map((items) => ({
-        value: items,
-        text: items,
-      }))}
-      onChange={(e) =>
-        dispatch({
-          type: Actions.SET_ITEMS_PER_PAGE,
-          payload: e.target.value,
-        })
-      }
-    />
+    <FlexBox gap="0.5rem" direction="row" justify="flex-end" align="center">
+      <label htmlFor={id}>Mostrar:</label>
+      <SelectGroupStyled
+        id={id}
+        name={id}
+        label="Mostrar"
+        value={itemsPerPage.toString()}
+        onChange={(e) =>
+          dispatch({
+            type: Actions.SET_ITEMS_PER_PAGE,
+            payload: e.target.value,
+          })
+        }
+      >
+        {['10', '25', '50'].map((items) => (
+          <option value={items} key={items}>
+            {items}
+          </option>
+        ))}
+      </SelectGroupStyled>
+    </FlexBox>
   )
 }
 
